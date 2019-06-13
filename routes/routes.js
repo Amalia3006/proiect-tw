5b29e9dc1af0f7180fa37bcfb3f39d82c19b83f0
bogdan9898 <bogdan_istoc2012@yahoo.com>
2019-06-13 06:10:19 +0300

    last changes

--- routes/routes.js
+++ routes/routes.js
@@ -37,59 +37,59 @@ const routesHandler = require("./routesHandler");
 const apiRoutesHandler = require("./apiRoutesHandler");
 
 const routesMapping = [
-  [/^\/*$/i, routesHandler.homeDisconnected],
-  [/^\/home\/[A-Za-z0-9]+\/*$/i, routesHandler.homeLoggedin],
-  // [/^\/home\/[A-Za-z0-9]+\/?$/i, routesHandler.homeDisconnected], // debug! delete me!
-  [/^\/login\/*$/i, routesHandler.login],
-  [/^\/signin\/*$/i, routesHandler.login],
-  [/^\/register\/*$/i, routesHandler.register],
-  [/^\/signup\/*$/i, routesHandler.register],
-  [/^\/logout\/*$/i, routesHandler.logout],
-  [/^\/signout\/*$/i, routesHandler.logout],
-  [/^\/profile\/[A-Za-z0-9]+\/*$/i, routesHandler.profile],
-  [/^\/jobs(\/[A-Za-z0-9]+)?\/*$/i, routesHandler.jobs],
-  [/^\/events(\/[A-Za-z0-9]+)?\/*$/i, routesHandler.events],
-  [/^\/image\/[A-Za-z0-9]+\/*$/i, routesHandler.image],
-  [/^\/image\/__[A-Za-z0-9%&@\-]+\/*$/i, routesHandler.imageResource],
-  [/^\/search\/?$/i, routesHandler.search]
+	[/^\/*$/i, routesHandler.homeDisconnected],
+	[/^\/home\/[A-Za-z0-9]+\/*$/i, routesHandler.homeLoggedin],
+	// [/^\/home\/[A-Za-z0-9]+\/?$/i, routesHandler.homeDisconnected], // debug! delete me!
+	[/^\/login\/*$/i, routesHandler.login],
+	[/^\/signin\/*$/i, routesHandler.login],
+	[/^\/register\/*$/i, routesHandler.register],
+	[/^\/signup\/*$/i, routesHandler.register],
+	[/^\/logout\/*$/i, routesHandler.logout],
+	[/^\/signout\/*$/i, routesHandler.logout],
+	[/^\/profile\/[A-Za-z0-9]+\/*$/i, routesHandler.profile],
+	[/^\/jobs(\/[A-Za-z0-9]+)?\/*$/i, routesHandler.jobs],
+	[/^\/events(\/[A-Za-z0-9]+)?\/*$/i, routesHandler.events],
+	[/^\/image\/[A-Za-z0-9]+\/*$/i, routesHandler.image],
+	[/^\/image\/__[A-Za-z0-9%&@\-]+\/*$/i, routesHandler.imageResource],
+	[/^\/search\/[A-Za-z0-9]+\/*$/i, routesHandler.search]
 ];
 
 function routes(request, response) {
-  const path = urlParse(request.url).pathname;
-  console.log("routes: " + request.method + " " + path);
+	const path = urlParse(request.url).pathname;
+	console.log("routes: " + request.method + " " + path);
 
-  for (item of routesMapping) {
-    // console.log("regex match: " + path + " @ " + item[0] + " => " + path.match(item[0]));
-    if (path.match(item[0])) {
-      item[1](request, response);
-      return;
-    }
-  }
+	for (item of routesMapping) {
+		// console.log("regex match: " + path + " @ " + item[0] + " => " + path.match(item[0]));
+		if (path.match(item[0])) {
+			item[1](request, response);
+			return;
+		}
+	}
 
-  send(404, response);
+	send(404, response);
 }
 
 const apiRoutesMapping = [
-  [/^\/api\/login\/?$/i, apiRoutesHandler.login],
-  [/^\/api\/register\/?$/i, apiRoutesHandler.register],
-  [/^\/api\/profile\/?$/i, apiRoutesHandler.profile],
-  [/^\/api\/jobs\/?$/i, apiRoutesHandler.jobs],
-  [/^\/api\/events\/?$/i, apiRoutesHandler.events]
+	[/^\/api\/login\/?$/i, apiRoutesHandler.login],
+	[/^\/api\/register\/?$/i, apiRoutesHandler.register],
+	[/^\/api\/profile\/?$/i, apiRoutesHandler.profile],
+	[/^\/api\/jobs\/?$/i, apiRoutesHandler.jobs],
+	[/^\/api\/events\/?$/i, apiRoutesHandler.events]
 ];
 
 function apiRoutes(request, response) {
-  const path = urlParse(request.url).pathname;
-  console.log("apiRoutes: " + request.method + " " + path);
+	const path = urlParse(request.url).pathname;
+	console.log("apiRoutes: " + request.method + " " + path);
 
-  for (item of apiRoutesMapping) {
-    // console.log("regex match: " + path + " @ " + item[0] + " => " + path.match(item[0]));
-    if (path.match(item[0])) {
-      item[1](request, response);
-      return;
-    }
-  }
+	for (item of apiRoutesMapping) {
+		// console.log("regex match: " + path + " @ " + item[0] + " => " + path.match(item[0]));
+		if (path.match(item[0])) {
+			item[1](request, response);
+			return;
+		}
+	}
 
-  send(404, response);
+	send(404, response);
 }
 
 module.exports = { routes, apiRoutes };
